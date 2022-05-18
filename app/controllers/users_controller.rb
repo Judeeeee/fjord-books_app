0e88fc3c96761b5239255f23620b70c443a1e5bf
Judeeeee <miku.nezasa@wiley-corp.com>
2022-05-18 09:33:11 +0900

    books_controllerを参考に、コントローラーを定義した

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,35 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: %i[show edit update]
+
   def index
+    @users = User.order(:id).page(params[:page])
   end
 
-  def show
-  end
+  def show; end
 
-  def edit
-  end
+  def edit; end
 
   def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to @user, notice: t('controllers.common.notice_update', name: User.model_name.human) }
+        format.json { render :show, status: :ok, location: @user }
+      else
+        format.html { render :edit }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_book
+    @book = User.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def book_params
+    params.require(:book).permit(:title, :memo, :author, :picture)
   end
 end
