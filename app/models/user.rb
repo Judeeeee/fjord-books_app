54f07da83b47382818d660f5d8efcbb146b7679f
Judeeeee <miku.nezasa@wiley-corp.com>
2022-05-18 14:55:30 +0900

    ページネーションを一覧ページに実装したかったので、kaminariを用いて実装した

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  paginates_per 5
+  max_paginates_per 100
+  max_pages 100
 end
