41b9ccf913ba80d66e4794c21b321a026b77e1d0
Judeeeee <miku.nezasa@wiley-corp.com>
2022-06-01 13:16:45 +0900

    followingテーブルとfollowerテーブルも沢山のUserrelationshipテーブルを持っているので、has_manyを設定した。ここで、素直にhas_many :userrelationship としてしまうと名前が被るので、名称を2つに分けた

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_one_attached :avatar
+  has_many :active_relationships, class_name: "Userrelationship"
+  has_many :passive_relationships, class_name: "Userrelationship"
 end
