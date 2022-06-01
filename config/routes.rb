4c238793cb85bcdf1a4ebea807ca118641479c3e
Judeeeee <miku.nezasa@wiley-corp.com>
2022-06-01 13:01:30 +0900

    /users/:is/userrelationshipsのネストでユーザーフォロー機能を指定したかったので、記述を加えた

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  get 'userrelationships/create'
-  get 'userrelationships/destroy'
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   devise_for :users
   root to: 'books#index'
   resources :books
   resources :users, only: %i(index show)
+  resources :users do
+    resource :userrelationships, only: [:create, :destroy]
+  end
 end
