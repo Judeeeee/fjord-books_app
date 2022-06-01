df9f4f8c5694465dc3d1d1d30aa5f2944eb11f55
Judeeeee <miku.nezasa@wiley-corp.com>
2022-06-01 12:42:28 +0900

    フォロー機能とフォロー解除機能を実装するために、userrelationshipsコントローラーを作成した
    rails g controller relationships create destroy

---
+++ app/controllers/userrelationships_controller.rb
@@ -0,0 +1,7 @@
+class UserrelationshipsController < ApplicationController
+  def create
+  end
+
+  def destroy
+  end
+end
---
+++ test/controllers/userrelationships_controller_test.rb
@@ -0,0 +1,13 @@
+require "test_helper"
+
+class UserrelationshipsControllerTest < ActionDispatch::IntegrationTest
+  test "should get create" do
+    get userrelationships_create_url
+    assert_response :success
+  end
+
+  test "should get destroy" do
+    get userrelationships_destroy_url
+    assert_response :success
+  end
+end
