0d5bc1f2659c90ee5bd34a708ba575ff8977aacc
Judeeeee <miku.nezasa@wiley-corp.com>
2022-06-01 12:40:18 +0900

    フォロイーとフォロワーのidをもつ中間テーブルを作成するために、userrelationshipモデルを作成した
    rails g model relationship following_id:integer follower_id:integer

---
+++ app/models/userrelationship.rb
@@ -0,0 +1,2 @@
+class Userrelationship < ApplicationRecord
+end
---
+++ test/models/userrelationship_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class UserrelationshipTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
