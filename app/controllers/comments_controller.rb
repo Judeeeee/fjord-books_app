31c944fb685dfacfbbd277825122b80cccc7c994
Judeeeee <miku.nezasa@wiley-corp.com>
2022-06-03 20:04:21 +0900

    投稿されたコメントが、この記述でブラウザで表示されるか確認するために変更を加えた

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,14 @@
 class CommentsController < ApplicationController
+  def create
+    @comment = Comment.new(comment_params)
+    @comment.save
+    debugger
+    redirect_to '/'
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:chat)
+  end
 end
