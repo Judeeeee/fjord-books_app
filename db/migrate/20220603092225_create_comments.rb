d1771339c2dedca09cdf212b55a813e19530f32f
Judeeeee <miku.nezasa@wiley-corp.com>
2022-06-03 19:52:26 +0900

    ここまでの変更をテーブルに反映させたいので、rails db:migrateを実行した

--- db/migrate/20220603092225_create_comments.rb
+++ db/migrate/20220603092225_create_comments.rb
@@ -2,7 +2,7 @@ class CreateComments < ActiveRecord::Migration[6.1]
   def change
     create_table :comments do |t|
       t.string :chat
-      t.references :postable, polymorphic: true, index: true
+      t.references :commentable, polymorphic: true, index: true
       t.timestamps
     end
   end
