3675e3c27983290ef9de207baf4b112eba32ec79
Judeeeee <miku.nezasa@wiley-corp.com>
2022-06-03 18:33:34 +0900

    コメントを構成する要素をコメントテーブルのカラムに定義したいので、変更を加えた

--- db/migrate/20220603092225_create_comments.rb
+++ db/migrate/20220603092225_create_comments.rb
@@ -1,7 +1,8 @@
 class CreateComments < ActiveRecord::Migration[6.1]
   def change
     create_table :comments do |t|
-
+      t.string :chat
+      t.references :postable, polymorphic: true, index: true
       t.timestamps
     end
   end
