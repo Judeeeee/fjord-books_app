5969664f448526e97d382f92bebca5551d5f330f
Judeeeee <miku.nezasa@wiley-corp.com>
2022-06-06 15:25:35 +0900

    投稿ユーザー名の入力有無によって、ブラウザに表示させる内容を変更させたかったので、記述を加えた

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,11 @@
 class CommentsController < ApplicationController
   def create
     @comment = @commentable.comments.build(comment_params)
+    if @comment.posted_user_name?
+      @comment.posted_user_name = @comment.posted_user_name
+    else
+      @comment.posted_user_name = current_user.email
+    end
     @comment.save
     redirect_to '/'
   end
@@ -8,6 +13,6 @@ class CommentsController < ApplicationController
   private
 
   def comment_params
-    params.require(:comment).permit(:chat)
+    params.require(:comment).permit(:chat, :posted_user_name)
   end
 end
