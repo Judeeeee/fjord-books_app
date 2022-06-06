7f0b8494dc73bf93ff87a04eca7ad9c412fff709
Judeeeee <miku.nezasa@wiley-corp.com>
2022-06-06 15:05:29 +0900

    テーブルへ実際にカラムを追加させるために、rails:db:migrateを実行した

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_03_092225) do
+ActiveRecord::Schema.define(version: 2022_06_06_060226) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 2022_06_03_092225) do
     t.integer "commentable_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "posted_user_name"
     t.index ["commentable_type", "commentable_id"], name: "index_comments_on_commentable"
   end
 
