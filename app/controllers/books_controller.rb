52c44b0b13807d1da7fef3367930e1e60fadcc3c
Judeeeee <miku.nezasa@wiley-corp.com>
2022-06-06 14:47:09 +0900

    一つのコメントフォームを使いまわしたいので、参考サイトを参照してコメント本文と投稿日時を表示させるテストを行った
    ref: https://qiita.com/28120671/items/98c07799b3d5548a1c4a

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -12,7 +12,6 @@ class BooksController < ApplicationController
   # GET /books/1
   # GET /books/1.json
   def show
-    @comment = Comment.new
     @book_comments = @book.comments
   end
 
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,5 @@
 class CommentsController < ApplicationController
   def create
-    @commentable = Book.find(params[:book_id])
     @comment = @commentable.comments.build(comment_params)
     @comment.save
     redirect_to '/'
