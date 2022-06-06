270662c65029f86e00167b527426082384f5dbc6
Judeeeee <miku.nezasa@wiley-corp.com>
2022-06-06 20:15:16 +0900

    rubocopを通すために削除した記述がないと投稿したコメントが保存されないため、記述を加えた。また、rubocopを通してpassできることを確認した

--- app/controllers/books/comments_controller.rb
+++ app/controllers/books/comments_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Books::CommentsController < CommentsController
+  before_action :set_commentable
   def set_commentable
     @commentable = Book.find(params[:book_id])
   end
