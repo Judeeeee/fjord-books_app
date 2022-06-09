f274add9291351829ac13aae7fd829df73b94ba9
Judeeeee <miku.nezasa@wiley-corp.com>
2022-06-09 13:58:11 +0900

    従来のメソッドだと、実行に失敗した場合にエラーが表示されず失敗かどうかがわからないため、修正を行った

--- app/controllers/user_relationships_controller.rb
+++ app/controllers/user_relationships_controller.rb
@@ -3,12 +3,12 @@
 class UserRelationshipsController < ApplicationController
   def create
     follow = current_user.active_relationships.build(follower_id: params[:user_id])
-    follow.save
+    follow.save!
     redirect_to users_path
   end
 
   def destroy
-    follow = current_user.active_relationships.find_by(follower_id: params[:user_id])
+    follow = current_user.active_relationships.find_by!(follower_id: params[:user_id])
     follow.destroy
     redirect_to users_path
   end
