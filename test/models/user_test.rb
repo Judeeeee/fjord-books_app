2801a1281c73e088f56c0502288b90a0ea05f448
Judeeeee <miku.nezasa@wiley-corp.com>
2022-06-09 17:18:08 +0900

    models/user.rbに定義されていたインスタンスメソッドのテストをしたいので、記述を加えた

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,40 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "フォローできるか？" do
+    alice= User.create!(email: 'alice@example', password: 'alice_password')
+    bob = User.create!(email: 'bob@example', password: 'bob_password')
+
+    alice.follow(bob)
+    assert alice.following?(bob)
+  end
+
+  test "フォローされているか？" do
+    alice= User.create!(email: 'alice@example', password: 'alice_password')
+    bob = User.create!(email: 'bob@example', password: 'bob_password')
+
+    alice.follow(bob)
+    assert bob.followed_by?(alice)
+  end
+
+  test "フォローを解除できるか？" do
+    alice= User.create!(email: 'alice@example', password: 'alice_password')
+    bob = User.create!(email: 'bob@example', password: 'bob_password')
+
+    alice.follow(bob)
+    alice.unfollow(bob)
+    assert_not alice.following?(bob)
+  end
+
+  test "ユーザー名を表すのに、登録した名前かemailのどちらかを選択する" do
+    user_email_is_exist = User.new(email: 'hoge@example', name: '')
+    assert_equal 'hoge@example', user_email_is_exist.name_or_email
+
+    user_name_is_exist = User.new(email: '', name: 'hoge')
+    assert_equal 'hoge', user_name_is_exist.name_or_email
+
+    user_name_is_double = User.new(email: 'hoge@example', name: 'hoge')
+    assert_equal 'hoge', user_name_is_exist.name_or_email
+  end
 end
