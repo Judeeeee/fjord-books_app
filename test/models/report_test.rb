c6c5ac6070700a4553eaddc985b349a35b67da52
Judeeeee <miku.nezasa@wiley-corp.com>
2022-06-09 19:17:49 +0900

    models/report.rbに定義されていたインスタンスメソッドのテストをしたいので、記述を加えた

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,7 +3,27 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "ログインユーザーが作成した日報なら編集できる" do
+    alice = User.create!(email: 'alice@example', password: 'alice_password')
+    alice_report = Report.create!(title: 'アリスの日報', content: '本文だよ', user:alice)
+
+    assert_equal(alice.id, alice_report.user_id)
+  end
+
+  test "ログインユーザーが作成した日報以外は編集できない" do
+    alice = User.create!(email: 'alice@example', password: 'alice_password')
+    bob = User.create!(email: 'bob@example', password: 'bob_password')
+    alice_report = Report.create!(title: 'アリスの日報', content: 'アリスの日報本文だよ', user:alice)
+    bob_report = Report.create!(title: 'ボブの日報', content: 'ボブの日報本文だよ', user:bob)
+
+    assert_not_equal(alice.id, bob_report.user_id)
+  end
+
+  test "日報作成時間を、yyyy/mm/dd表記にする" do
+    alice = User.create!(email: 'alice@example', password: 'alice_password')
+    alice_report = Report.create!(title: 'アリスの日報', content: 'アリスの日報本文だよ', user:alice)
+    today_time = Time.new.to_date
+
+    assert_equal(alice_report.created_at.to_date, today_time)
+  end
 end
