ce2129354e98d90747a9501417f565d382da74d7
Judeeeee <miku.nezasa@wiley-corp.com>
2022-06-10 16:45:18 +0900

    以前の場合、userrelationテーブルを先に作成=>テーブル名の変更と不必要なステップを踏んでいた。そのため、元のファイルに変更をまとめて記述した

--- db/migrate/20220609022911_change_userrelationships_to_user_relationships.rb
+++
@@ -1,5 +0,0 @@
-class ChangeUserrelationshipsToUserRelationships < ActiveRecord::Migration[6.1]
-  def change
-    rename_table :userrelationships, :user_relationships
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_09_022911) do
+ActiveRecord::Schema.define(version: 2022_06_01_033958) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
